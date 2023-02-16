b5e3d9ae31a2c6d6c89fdb1a595d9ac91f0d5a14
enkud <enkudinov222@gmail.com>
2023-02-16 20:16:25 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,12 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int age = 18;
+        int age = 17;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
-        }
+        } else { System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");}
     }
+
 
     public static void task2() {
         System.out.println("Задача 2");
